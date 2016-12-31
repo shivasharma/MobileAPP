2bd2859852d9c8df6ff2cb01aee383ccfef4711c
shiva sharma <shivathebravo@gmail.com>
2016-12-31 12:02:48 -0500

    Added code for contact

--- MobileAPP/MobileAPP/Contact.xaml.cs
+++ MobileAPP/MobileAPP/Contact.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Plugin.Messaging;
 using Xamarin.Forms;
 
 namespace MobileAPP
@@ -14,5 +14,24 @@ namespace MobileAPP
         {
             InitializeComponent();
         }
+
+        private void Button_OnClicked(object sender, EventArgs e)
+        {
+            var emailMessage = CrossMessaging.Current.EmailMessenger;
+           
+               // emailMessage.SendEmail("info@mstl.us","Contact Us from Mobile","This is message from mobile");
+
+                var email = new EmailMessageBuilder()
+                    .To("info@mstl.us")
+                    .Subject("Contact Us from mobile")
+                    .Body(message.Text)
+                    .Build();
+                emailMessage.SendEmail(email);
+
+
+
+            
+        }
+
     }
 }
